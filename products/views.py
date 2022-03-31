bd6298f5efcd49311651d090f35cbab20f22ddec
Lizeth <mendezlizeth084@gmail.com>
2022-03-31 14:33:52 -0700

    Created Delete Functionality for Product App in Views File

--- products/views.py
+++ products/views.py
@@ -26,11 +26,11 @@ def products_list(request):
         return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request,pk):
 
     product= get_object_or_404(Product, pk=pk)
-    
+
     if request.method == 'GET':
         serializer= ProductSerializer(product)
         return Response(serializer.data)
@@ -40,6 +40,10 @@ def product_detail(request,pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     
 
